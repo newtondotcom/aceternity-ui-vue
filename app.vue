2e302193d1677541afc00c58a8aefb4d1d913f86
newtondotcom <asphalt9fr@gmail.com>
2024-07-03 15:15:13 +0200

    template do display components

--- app.vue
+++ app.vue
@@ -1,10 +1,5 @@
 <template>
-  <div>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
 
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+<NuxtPage />
+
 </template>
