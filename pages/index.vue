3cae36b255c58e52ee368c39254900eb314d11b5
newtondotcom <asphalt9fr@gmail.com>
2024-07-03 15:24:08 +0200

    feat : page to list components started, for failures and success

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+
+<h1>This is a work in progress</h1>
+
+<ul>
+
+<li><a href="https://ui.aceternity.com/components/3d-card-effect">3D Card Effect</a> : <a href="/3d-card-effect">Working</a></li>
+<li><a href="https://ui.aceternity.com/components/macbook-scroll">Macbook Scroll</a> : <a href="/macbook-scroll">Working</a></li>
+
+
+</ul>
+</template>
