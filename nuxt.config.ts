1be7db56dd2ea26fb020045efd4b5fb3aafeb861
newtondotcom <asphalt9fr@gmail.com>
2024-08-26 00:39:22 +0200

    chore : render md of the readme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxtjs/mdc'],
   shadcn: {
     prefix: '',
     componentDir: './components/ui'
