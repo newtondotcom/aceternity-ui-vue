db7ce35f9dd34184bf19a18c97a79f9ac9824a87
newtondotcom <asphalt9fr@gmail.com>
2024-08-26 00:25:25 +0200

    feat : new spotlight component

---
+++ components/aceternity-ui/spotlight/Spotlight.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+    import { cn } from '../../../lib/utils';
+    interface SpotlightProps {
+    className?: string;
+    fill?: string;
+    }
+    const props = defineProps<SpotlightProps>();
+</script>
+
+<template>
+<svg
+	:class="cn(
+		'pointer-events-none absolute z-[1] h-[169%] w-[138%] animate-spotlight opacity-0 lg:w-[84%]',
+		className
+	)"
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 3787 2842"
+	fill="none"
+>
+	<g filter="url(#filter)">
+		<ellipse
+			cx="1924.71"
+			cy="273.501"
+			rx="1924.71"
+			ry="273.501"
+			transform="matrix(-0.822377 -0.568943 -0.568943 0.822377 3631.88 2291.09)"
+			class="text-primary"
+			:fill="fill || 'white'"
+			fill-opacity="0.21"
+		></ellipse>
+	</g>
+	<defs>
+		<filter
+			id="filter"
+			x="0.860352"
+			y="0.838989"
+			width="3785.16"
+			height="2840.26"
+			filterUnits="userSpaceOnUse"
+			color-interpolation-filters="sRGB"
+		>
+			<feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood>
+			<feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"></feBlend>
+			<feGaussianBlur stdDeviation="151" result="effect1_foregroundBlur_1065_8"></feGaussianBlur>
+		</filter>
+	</defs>
+</svg>
+</template>
---
+++ pages/spotlight.vue
@@ -0,0 +1,22 @@
+<template>
+<div
+	class="relative flex h-[25rem] w-full flex-col overflow-hidden rounded-md bg-white/[0.96] antialiased bg-grid-white/[0.02] dark:bg-black/[0.96] md:h-[40rem] md:items-center md:justify-center"
+>
+	<Spotlight className="-top-40 left-0 md:left-60 md:-top-20" fill="currentColor" />
+	<div class=" relative z-10 mx-auto w-full max-w-7xl p-4 pt-20 md:pt-0">
+		<h1
+			class="bg-opacity-50 bg-gradient-to-b from-neutral-400 to-neutral-700 bg-clip-text text-center text-4xl font-bold text-transparent dark:from-neutral-50 dark:to-neutral-400 md:text-7xl"
+		>
+			spotlight
+		</h1>
+		<h2
+			class="bg-opacity-50 text-neutral-600 text-center text-2xl font-bold dark:from-neutral-50 dark:to-neutral-400 md:text-2xl"
+		>
+			wait the spotlight is coming
+		</h2>
+	</div>
+</div>
+</template>
+<script setup lang="ts">
+import Spotlight from '../components/aceternity-ui/spotlight/Spotlight.vue';
+</script>
